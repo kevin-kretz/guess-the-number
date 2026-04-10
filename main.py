be34ec15d39f33d611697bfe78e820e16ff2cc9f
Kevin Kretz <kkretz17@gmail.com>
2026-04-09 21:36:47 -0400

    removed duplicate code. removed previous_guess var. renamed current_guess var to guess.

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import random
 
-goal_number = random.randint(1,100)
-current_guess = int(input("What is your guess? "))
-
-if current_guess < goal_number:
-    print("Higher...")
-else:
-    print("Lower...")
-
-while current_guess != goal_number:
-    previous_guess = current_guess
-    current_guess = int(input("Next guess: "))
-
-    if current_guess < goal_number:
+def print_hint(guess):
+    if guess < goal_number:
         print("Higher...")
     else:
         print("Lower...")
+
+goal_number = random.randint(1,100)
+guess = int(input("What is your guess? "))
+print_hint(guess)
+
+while guess != goal_number:
+    guess = int(input("Next guess: "))
+
+    print_hint(guess)
 
 print(f"You did it! The number was {goal_number}")
